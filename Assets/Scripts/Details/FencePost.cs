5bad3de4d36984fd17c5d65111fbc69ad816c631
Noel Berry <thenberry@gmail.com>
2013-09-29 23:37:57 -0700

    added fences

---
+++ Assets/Scripts/Details/FencePost.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FencePost : MonoBehaviour {
+	
+	tk2dSprite sprite;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+
+}

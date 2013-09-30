f52cab339d66ab2c4875b314bb6b943fb2c12ce9
ChevyRay <happytrash@gmail.com>
2013-09-30 05:48:08 -0700

    added batch menu item

---
+++ Assets/Scripts/Utilities/Editor/BatchEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public static class BatchEditor
+{
+	[MenuItem("GameObject/Combine Meshes")]
+	static void CombineMeshes()
+	{
+		Batch.CombineMeshes(Selection.activeGameObject);
+	}
+	[MenuItem("GameObject/Combine Meshes", true)]
+	static bool ValidateCombineMeshes()
+	{
+		return Selection.activeGameObject != null;
+	}
+}

4292def8cacc2e6bf1f025106d1d17239d1e8533
ChevyRay <happytrash@gmail.com>
2013-09-30 03:41:20 -0700

    Added visibility manager to trees.

---
+++ Assets/Scripts/Details/VisibilityManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VisibilityManager : Singleton<VisibilityManager>
+{
+	public float updateFrequency = 0.1f;
+	[HideInInspector] public List<VisibilityObject> objects = new List<VisibilityObject>();
+	
+	void Start()
+	{
+		StartCoroutine(UpdateVisibility());
+	}
+	
+	IEnumerator UpdateVisibility()
+	{
+		while (true)
+		{
+			yield return StartCoroutine(Auto.Wait(updateFrequency));
+			foreach (var obj in objects)
+			{
+				var pos = Camera.main.WorldToScreenPoint(obj.transform.localPosition);
+				obj.SetVisible(pos.y > -50);
+			}
+		}
+	}
+}

67739bcbacba161128f2799725656e62ec408b67
ChevyRay <happytrash@gmail.com>
2013-09-29 23:59:49 -0700

    Added camera script.

---
+++ Assets/Scripts/GameCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCamera : Singleton<GameCamera>
+{
+	public float moveRate;
+	
+	Transform target;
+	
+	public void SetTarget(Transform target)
+	{
+		this.target = target;
+	}
+	
+	void Update()
+	{
+		transform.position = Vector3.Lerp(transform.position, target.position, moveRate * Time.deltaTime);
+	}
+}

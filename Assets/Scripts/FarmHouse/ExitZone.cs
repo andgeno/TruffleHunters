ef7ffff1a79967c5c95c301c8ef66b47f67c9f94
ChevyRay <happytrash@gmail.com>
2013-09-30 18:45:14 -0700

    added game data object

---
+++ Assets/Scripts/FarmHouse/ExitZone.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ExitZone : Singleton<ExitZone>
+{
+	public List<Pig> pigs = new List<Pig>();
+	
+	void OnTriggerEnter(Collider other)
+	{
+		var pig = other.GetComponent<Pig>();
+		if (pig != null)
+			pigs.Add(pig);
+	}
+	
+	void OnTriggerExit(Collider other)
+	{
+		var pig = other.GetComponent<Pig>();
+		if (pig != null)
+			pigs.Remove(pig);
+	}
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Xml;
+
+public class Game : Singleton<Game>
+{
+	static XmlSerializer serializer;
+	
+	public GameData data = new GameData();
+	
+	protected override void Awake()
+	{
+		base.Awake();
+		Load();
+	}
+	
+	public static void Save()
+	{
+		if (serializer == null)
+			serializer = new XmlSerializer(typeof(GameData));
+		var writer = XmlWriter.Create("GameData.xml");
+		serializer.Serialize(writer, instance.data);
+	}
+	
+	public static void Load()
+	{
+		if (serializer == null)
+			serializer = new XmlSerializer(typeof(GameData));
+		var reader = XmlReader.Create("GameData.xml");
+		instance.data = (GameData)serializer.Deserialize(reader);
+	}
+}

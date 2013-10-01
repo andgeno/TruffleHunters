dfa97e45add9f506de74e1806d17e6162e7c8743
ChevyRay <happytrash@gmail.com>
2013-09-30 19:03:24 -0700

    working on screen fade

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exit : MonoBehaviour
+{
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.GetComponent<Player>() != null)
+		{
+			
+			enabled = false;
+		}
+	}
+}
--- Assets/Scripts/GameCamera.cs
+++ Assets/Scripts/GameCamera.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class GameCamera : Singleton<GameCamera>
 {
+	public Texture pixel;
+	public float fadeTime;
+	[HideInInspector] public Color color;
+	
 	void Start()
 	{
 		Camera.main.transparencySortMode = TransparencySortMode.Orthographic;
@@ -14,13 +18,37 @@ public class GameCamera : Singleton<GameCamera>
 	{
 		StartCoroutine(DoFollowTarget(target));
 	}
-	
 	IEnumerator DoFollowTarget(Transform target)
 	{
 		while (true)
 		{
 			transform.position = target.position;
 			yield return 0;
+		}
+	}
+	
+	public void FadeToScene(string name)
+	{
+		
+	}
+	IEnumerator DoFadeToScene(string name)
+	{
+		color = new Color(0, 0, 0, 0);
+		var fadeSpeed = 1 / fadeTime;
+		while (!Mathf.Approximately(color.a, 1))
+		{
+			color.a = Mathf.MoveTowards(color.a, 1, fadeSpeed * Time.deltaTime);
+			yield return 0;
+		}
+		Application.LoadLevel(name);
+	}
+	
+	void OnGUI()
+	{
+		if (!Mathf.Approximately(color.a, 0))
+		{
+			GUI.color = color;
+			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), pixel);
 		}
 	}
 }

d5a0252f684be501f889853c14ddb786e26b031e
Drazzke <thenberry@gmail.com>
2013-09-30 19:00:55 -0700

    Added entrance to forest, added dead trees/stumps

--- Assets/Scripts/ForestGenerator.cs
+++ Assets/Scripts/ForestGenerator.cs
@@ -16,12 +16,17 @@ public class ForestGenerator : MonoBehaviour
 	
 	public float grassTurfPerMeter;
 	
+	public Transform playerPrefab;
+	
 	public PolyMesh ground;
 	public Transform treePrefab;
+	public Transform deadTreePrefab;
+	public Transform stumpPrefab;
 	public Transform rockPrefab;
 	public Transform hFencePrefab;
 	public Transform vFencePrefab;
 	public Transform grassTurfPrefab;
+	public Transform gatePrefab;
 	
 	public Transform treeRoot;
 	public Transform rockRoot;
@@ -30,6 +35,8 @@ public class ForestGenerator : MonoBehaviour
 	
 	void Awake()
 	{
+		// spawn the player
+		playerPrefab.Spawn(new Vector3(0, 0, size.y - fencePadding.y));
 	}
 	
 	void Start()
@@ -46,17 +53,31 @@ public class ForestGenerator : MonoBehaviour
 		ground.keyPoints[3] = new Vector3(-size.x, size.y);
 		ground.BuildMesh();
 		
-		// spawn the perimeter
-		for (float i = - size.x + fencePadding.x; i < size.x - fencePadding.x; i += hFencePerMeter)
-		{
-			hFencePrefab.Spawn(new Vector3(i, 0, -size.y + fencePadding.y)).parent = fenceRoot;
+		// top perimeter
+		for (float i = 2; i < size.x - fencePadding.x; i += hFencePerMeter)
 			hFencePrefab.Spawn(new Vector3(i, 0, size.y - fencePadding.y)).parent = fenceRoot;
-		}
+		for (float i = -2 - hFencePerMeter; i > -(size.x - fencePadding.x + hFencePerMeter); i -= hFencePerMeter)
+			hFencePrefab.Spawn(new Vector3(i, 0, size.y - fencePadding.y)).parent = fenceRoot;
+		
+		// spawn the bottom perimeter
+		for (float i = - size.x + fencePadding.x; i < size.x - fencePadding.x; i += hFencePerMeter)
+			hFencePrefab.Spawn(new Vector3(i, 0, -size.y + fencePadding.y)).parent = fenceRoot;
+		
+		// side parimeters
 		for (float i = - size.y + fencePadding.y; i < size.y - fencePadding.y; i += vFencePerMeter)
 		{
 			vFencePrefab.Spawn(new Vector3(-size.x + fencePadding.x, 0, i)).parent = fenceRoot;
 			vFencePrefab.Spawn(new Vector3(size.x - fencePadding.x, 0, i)).parent = fenceRoot;
 		}
+		
+		// place edge entrace fence
+		for (float i = 0; i < fencePadding.y + vFencePerMeter; i += vFencePerMeter)
+		{
+			vFencePrefab.Spawn(new Vector3(-2, 0, size.y - i)).parent = fenceRoot;	
+			vFencePrefab.Spawn(new Vector3(2, 0, size.y - i)).parent = fenceRoot;	
+		}
+		
+		gatePrefab.Spawn (new Vector3(-1.5f, 0, size.y));
 		
 		// generate trees
 		Vector2 treePerlinOffset = new Vector2(Rand.Float(10000), Rand.Float(10000));
@@ -64,30 +85,35 @@ public class ForestGenerator : MonoBehaviour
 		{
 			for (float j = - size.y; j < size.y; j += 2)
 			{
-				// make sure the root of the tree/rock doesn't overlap the fence
-				float fenceDistX = 0.60f;
-				float fenceDistY = 0.10f;
-				
-				if ((i < -size.x + fencePadding.x - fenceDistX || i > -size.x + fencePadding.x + fenceDistX)
-					&& (i < size.x - fencePadding.x - fenceDistX || i > size.x - fencePadding.x + fenceDistX)
-					&& (j < -size.y + fencePadding.y - fenceDistY || j > -size.y + fencePadding.y + fenceDistY)
-					&& (j < size.y - fencePadding.y - fenceDistY || j > size.y - fencePadding.y + fenceDistY))
+				// don't spawn near entrance
+				if (!(j > size.y - 20 && i > -4 && i < 4))
 				{
-					float sample = Mathf.PerlinNoise(treePerlinOffset.x + i * treePerlinScale, treePerlinOffset.y + j * treePerlinScale);
-					Vector3 stagger = new Vector3(Rand.Float(-0.50f, 0.50f), 0, Rand.Float(-0.50f, 0.50f));
+					// make sure the root of the tree/rock doesn't overlap the fence
+					float fenceDistX = 0.60f;
+					float fenceDistY = 0.10f;
 					
-					if (treeThreshold > sample)
+					// don't spawn near fence
+					if ((i < -size.x + fencePadding.x - fenceDistX || i > -size.x + fencePadding.x + fenceDistX)
+						&& (i < size.x - fencePadding.x - fenceDistX || i > size.x - fencePadding.x + fenceDistX)
+						&& (j < -size.y + fencePadding.y - fenceDistY || j > -size.y + fencePadding.y + fenceDistY)
+						&& (j < size.y - fencePadding.y - fenceDistY || j > size.y - fencePadding.y + fenceDistY))
 					{
-						if (Rand.Chance(treeInnerPercentage))
-							treePrefab.Spawn(new Vector3(i, 0, j) + stagger).parent = treeRoot;
-					}
-					else
-					{
-						if (sample - treeThreshold < 0.01f)
-							rockPrefab.Spawn(new Vector3(i, 0, j) + stagger).parent = rockRoot;
+						float sample = Mathf.PerlinNoise(treePerlinOffset.x + i * treePerlinScale, treePerlinOffset.y + j * treePerlinScale);
+						Vector3 stagger = new Vector3(Rand.Float(-0.50f, 0.50f), 0, Rand.Float(-0.50f, 0.50f));
 						
-						if (Rand.Chance(treeOuterPercentage))
-							treePrefab.Spawn(new Vector3(i, 0, j) + stagger).parent = treeRoot;
+						if (treeThreshold > sample)
+						{
+							if (Rand.Chance(treeInnerPercentage))
+								SetTree (new Vector3(i, 0, j) + stagger);
+						}
+						else
+						{
+							if (sample - treeThreshold < 0.01f)
+								rockPrefab.Spawn(new Vector3(i, 0, j) + stagger).parent = rockRoot;
+							
+							if (Rand.Chance(treeOuterPercentage))
+								SetTree (new Vector3(i, 0, j) + stagger);
+						}
 					}
 				}
 			}
@@ -98,8 +124,16 @@ public class ForestGenerator : MonoBehaviour
 		for (int i = 0; i < grassTurfCount; i ++)
 			grassTurfPrefab.Spawn(new Vector3(Rand.Float(-size.x, size.x), 0, Rand.Float(-size.y, size.y))).parent = grassRoot;
 		
-		
-		
 		yield return 0;
+	}
+	
+	void SetTree(Vector3 position)
+	{
+		if (Rand.Chance(0.90f))
+			treePrefab.Spawn(position).parent = treeRoot;
+		else if (Rand.Chance(0.50f))
+			deadTreePrefab.Spawn(position).parent = treeRoot;
+		else
+			stumpPrefab.Spawn(position).parent = treeRoot;
 	}
 }

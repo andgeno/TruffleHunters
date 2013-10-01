87048fca4356614a7962c0b22bb8fa5058d60502
ChevyRay <happytrash@gmail.com>
2013-09-30 19:12:08 -0700

    Added scene exits and fading in/out between scenes.

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class Exit : MonoBehaviour
 {
+	public string scene;
+	
+	void Awake()
+	{
+		if (renderer != null)
+			Destroy(renderer);
+	}
+	
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.GetComponent<Player>() != null)
 		{
-			
+			GameCamera.instance.FadeToScene(scene);
 			enabled = false;
 		}
 	}
